a0aef2ef00f2ec23536608222409fbfd0100cf38
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-03-20 16:46:04 +0500

    Created first activity

--- app/src/main/java/com/example/nerdlauncher/NerdLauncherActivity.kt
+++ app/src/main/java/com/example/nerdlauncher/NerdLauncherActivity.kt
@@ -1,11 +1,19 @@
 package com.example.nerdlauncher
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class NerdLauncherActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nerd_launcher)
+
+        recyclerView = findViewById(R.id.app_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
